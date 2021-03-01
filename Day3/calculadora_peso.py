a89abf45097417c536445b58499c50915aeaa2af
Moises Ariza <arizamoises@gmail.com>
2021-02-28 21:58:23 -0500

    Dia tres calculadora de peso y busqueda del tesoro

---
+++ Day3/calculadora_peso.py
@@ -0,0 +1,17 @@
+print("Bienvenido a la calculadora de peso, vamos a ver si estas en tu peso ideal\n")
+print("Ingresa los siguientes datos solicitados:\n")
+peso = float(input("ingrese su peso actual: "))
+altura = float(input("Ingrese su altura actual: "))
+
+imc = round(peso / (altura **2))
+
+if imc <= 18.5:
+    print("Cuidado tiene bajo peso")
+elif imc > 18.5 and imc < 25:
+    print("Su peso es normal")
+elif imc > 25 and imc < 30:
+    print("Ojo esta en sobrepeso")
+elif imc > 30 and imc < 35:
+    print("Cuidado ya esta obeso")
+else:
+    print("Estas muy obeso ve al doctor que te puedes morir")
