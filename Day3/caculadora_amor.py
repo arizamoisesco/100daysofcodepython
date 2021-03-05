ef2cb178cadf9fe49f200b7eca9c36ee284f917d
Moises <arizamoises@gmail.com>
2021-03-04 23:07:54 -0500

    Prueba de mejoramiento para ajustarlo a la condicion dada. Ojo este commit tiene erro y da lo que deberia dar xD

--- Day3/caculadora_amor.py
+++ Day3/caculadora_amor.py
@@ -4,27 +4,49 @@ nombre1 = nombre1.lower()
 nombre2 = input("¿Cuál es su nombre?: ")
 nombre2 = nombre2.lower()
 
-def calculo_amor(nombre):
-    #Contar True
+def calculo_amor(nombre, nombre2):
+    #Contar True primer nombre
     t = nombre.count("t")
     r = nombre.count("r")
     u = nombre.count("u")
     e = nombre.count("e")
-    #Contar Love
+    nombre1_true = t+r+u+e
+
+    #Contar True segundo nombre
+    t = nombre.count("t")
+    r = nombre.count("r")
+    u = nombre.count("u")
+    e = nombre.count("e")
+    nombre2_true = t+r+u+e
+
+    total_true = str(nombre1_true + nombre2_true)
+    #Contar Love primer nombre
     l = nombre.count("l")
     o = nombre.count("o")
     v = nombre.count("v")
     e = nombre.count("e")
+    nombre1_love = l+o+v+e
+
+    #Contar Love segundo nombre
+    l = nombre.count("l")
+    o = nombre.count("o")
+    v = nombre.count("v")
+    e = nombre.count("e")
+    nombre2_love = l+o+v+e
+
+    total_love = str(nombre1_love + nombre2_love)
     
-    total = t+r+u+e+l+o+v+e
+    total = int(total_true+total_love)
     return total
 
+'''
 total_nombre1 = calculo_amor(nombre1)
 print(total_nombre1)
 total_nombre2 = calculo_amor(nombre2)
 print(total_nombre2)
+'''
 
-puntaje_amor = total_nombre1 + total_nombre2
+puntaje_amor = calculo_amor(nombre1, nombre2)
 
 if puntaje_amor < 10 or puntaje_amor > 90:
     print(f"Tu puntaje es {puntaje_amor}, vamos juntos por helado :(")
