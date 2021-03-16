eb039d9a6453db7a539bf378e40914b9efe8e7b5
Moises <arizamoises@gmail.com>
2021-03-15 22:10:21 -0500

    Se refactorizó el código para disminuir las lineas usadas y se ajusto para ahora si hacer el calculo como es de la calculadora del amor :D

--- Day3/caculadora_amor.py
+++ Day3/caculadora_amor.py
@@ -1,53 +1,33 @@
 print("Bienvenido a la calculadora del amor")
 nombre1 = input("¿Cuál es su nombre?: ")
-nombre1 = nombre1.lower()
-nombre2 = input("¿Cuál es su nombre?: ")
-nombre2 = nombre2.lower()
+nombre2 = input("¿Cuál es el nombre de tu Crush?: ")
 
 def calculo_amor(nombre, nombre2):
+    #Combinemos los nombres para ser mas rapido en el conteo
+    ustedes = nombre + nombre2
+    ustedes = ustedes.lower()
+
     #Contar True primer nombre
-    t = nombre.count("t")
-    r = nombre.count("r")
-    u = nombre.count("u")
-    e = nombre.count("e")
-    nombre1_true = t+r+u+e
+    t = ustedes.count("t")
+    r = ustedes.count("r")
+    u = ustedes.count("u")
+    e = ustedes.count("e")
+    total_true = t+r+u+e
 
-    #Contar True segundo nombre
-    t = nombre.count("t")
-    r = nombre.count("r")
-    u = nombre.count("u")
-    e = nombre.count("e")
-    nombre2_true = t+r+u+e
-
-    total_true = str(nombre1_true + nombre2_true)
     #Contar Love primer nombre
-    l = nombre.count("l")
-    o = nombre.count("o")
-    v = nombre.count("v")
-    e = nombre.count("e")
-    nombre1_love = l+o+v+e
-
-    #Contar Love segundo nombre
-    l = nombre.count("l")
-    o = nombre.count("o")
-    v = nombre.count("v")
-    e = nombre.count("e")
-    nombre2_love = l+o+v+e
-
-    total_love = str(nombre1_love + nombre2_love)
+    l = ustedes.count("l")
+    o = ustedes.count("o")
+    v = ustedes.count("v")
+    e = ustedes.count("e")
+    total_love = l+o+v+e
     
-    total = int(total_true+total_love)
-    return total
+    #Convertimos los resultados de true y love en String para concatenarlos
+    total_puntaje = str(total_true)+str(total_love)
+    return total_puntaje
 
-'''
-total_nombre1 = calculo_amor(nombre1)
-print(total_nombre1)
-total_nombre2 = calculo_amor(nombre2)
-print(total_nombre2)
-'''
 
 puntaje_amor = calculo_amor(nombre1, nombre2)
-
+puntaje_amor = int(puntaje_amor)
 if puntaje_amor < 10 or puntaje_amor > 90:
     print(f"Tu puntaje es {puntaje_amor}, vamos juntos por helado :(")
 elif puntaje_amor > 40 and puntaje_amor < 50:
