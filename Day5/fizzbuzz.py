8083e2bbdb948ed2ae95d999d1056fa555a24ec7
Moises <arizamoises@gmail.com>
2021-03-24 16:50:56 -0500

    Primera versión del código, no detecta los multiplos de 15

---
+++ Day5/fizzbuzz.py
@@ -0,0 +1,15 @@
+#Generamos los 100 números y guardarlos en un array
+
+for numero in range(1, 101):
+    
+    if numero % 3 == 0:
+        print("Fizz")
+    elif numero % 5 == 0:
+        print("Buzz")
+    elif numero % 15 == 0:
+        print("FizzBuzz")
+    else:
+        print(numero)
+#Verificamos si son divisibles por 3, si es el caso lanzar un menzaje que diga "fizz"
+#Verificamos si es divisible por 5, si es el caso lanzamos un mensaje que diga "Buzz"
+#Verificamos si es divisible por 15, entonces lanzamos el mensaje "Fizzbuzz"
