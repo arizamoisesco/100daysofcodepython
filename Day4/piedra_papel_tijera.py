bb4090c15b44096914290a57d825935714d2c2f2
Moises <arizamoises@gmail.com>
2021-03-29 21:11:08 -0500

    Modificación al script de piedra, papel y tijeras. Se agrego las imagenes ASCII de las manos

--- Day4/piedra_papel_tijera.py
+++ Day4/piedra_papel_tijera.py
@@ -3,7 +3,35 @@ import random
 print("Bienvenido al juego de piedra, papel y tijera")
 mano_usuario = int(input("¿Cuál opción deseas? Escribe piedra(0), papel(1), tijeras(2)"))
 mano_cpu = random.randint(0, 2)
-opciones = "Piedra", "Papel", "Tijera"
+
+piedra = '''
+
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+papel = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+tijeras = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+opciones = f"Piedra {piedra}", f"Papel{papel}", f"Tijera {tijeras}"
 print(f"Tu escogiste {opciones[mano_usuario]} y el computador {opciones[mano_cpu]}")
 
 if(mano_usuario == 0 and mano_cpu == 0):
