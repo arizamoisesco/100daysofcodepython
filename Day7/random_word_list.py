565e16da74a096716c80ca4947c419f496c62af2
Moises <arizamoises@gmail.com>
2021-04-06 13:10:37 -0500

    Modificación script

--- Day7/random_word_list.py
+++ Day7/random_word_list.py
@@ -3,7 +3,6 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-numero_aleatoria = random.randint(0, len(word_list)-1)
 palabra_aleatoria = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
