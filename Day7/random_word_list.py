5279d1bdbc8a74a076d904542ebec3bff6d7cea8
Moises <arizamoises@gmail.com>
2021-04-06 13:05:58 -0500

    Script que escoge de una lista palabras aleatorias y las compara con la letra ingresada por el usuario

---
+++ Day7/random_word_list.py
@@ -0,0 +1,17 @@
+#Step 1 
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+numero_aleatoria = random.randint(0, len(word_list)-1)
+palabra_aleatoria = word_list[numero_aleatoria]
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+letra_usuario = input("Ingrese la letra: ").lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letras in palabra_aleatoria:
+    if letras == letra_usuario:
+        print("Bien")
+    else:
+        print("Mal")
