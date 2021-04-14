aa6b5c1958879ce39f3debb007312a9213b000f0
Moises Ariza <arizamoises@gmail.com>
2021-04-14 17:11:27 -0500

    Se integro dentro del código los modulos que se habian importado para su respectivo uso

--- Day7/random_word_list.py
+++ Day7/random_word_list.py
@@ -1,63 +1,12 @@
 #Step 1 
 import random
-estados = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import arte_ahorcado as dibujos
+import palabras_ahorcado as palabras
 
-word_list = ["aardvark", "baboon", "camel"]
+print(dibujos.logo)
+estados = dibujos.stages
+
+word_list = palabras.word_list
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 palabra_aleatoria = random.choice(word_list)
