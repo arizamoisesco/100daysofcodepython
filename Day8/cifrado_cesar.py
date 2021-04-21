e11051d0bd5791b09bcf0a437259e51450110651
Moises Ariza <arizamoises@gmail.com>
2021-04-20 22:05:03 -0500

    Se depura el codigo para que solo tenga una funcion

--- Day8/cifrado_cesar.py
+++ Day8/cifrado_cesar.py
@@ -1,55 +1,25 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-    text_encode = ""
+def ceaser(text, shift, direction):
+    text_plan = ""
     position_alphabet = 0
     len_alphabet = len(alphabet)-1
     for letter in text:
-        position_alphabet  = alphabet.index(letter) + shift
-
+        if direction == "encode":
+            position_alphabet  = alphabet.index(letter) + shift
+        elif direction == "decode":
+            position_alphabet  = alphabet.index(letter) - shift
+    
         if position_alphabet > len_alphabet:
             position_alphabet = 0
 
-        text_encode += alphabet[position_alphabet]
+        text_plan += alphabet[position_alphabet]
+    print(f"The message {direction}d is {text_plan}")
 
-    print(f"EL mensaje códificado es: {text_encode}")
+ceaser(text, shift, direction)
 
-def decrypt(text, shift):
-    text_decode = ""
-    position_alphabet = 0
-    len_alphabet = len(alphabet)-1
-    for letter in text:
-        position_alphabet  = alphabet.index(letter) - shift
-
-        if position_alphabet > len_alphabet:
-            position_alphabet = 0
-
-        text_decode += alphabet[position_alphabet]
-
-    print(f"EL mensaje cifrado es: {text_decode}")
-
-
-
-if(direction == 'encode'):  
-    encrypt(text, shift)
-elif(direction == 'decode'):
-    decrypt(text, shift)
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a messag
+  
