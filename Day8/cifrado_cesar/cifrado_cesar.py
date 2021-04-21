6b85528ad51c0e82aefa41f14a75426d5dcbb17a
Moises Ariza <arizamoises@gmail.com>
2021-04-20 22:57:43 -0500

    Se organiza por carpeta y agrega el arte y repeticion del codigo

---
+++ Day8/cifrado_cesar/cifrado_cesar.py
@@ -0,0 +1,32 @@
+import art
+print(art.logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def ceaser(text, shift, direction):
+    text_plan = ""
+    position_alphabet = 0
+    len_alphabet = len(alphabet)-1
+    for letter in text:
+        if direction == "encode":
+            position_alphabet  = alphabet.index(letter) + shift
+        elif direction == "decode":
+            position_alphabet  = alphabet.index(letter) - shift
+    
+        if position_alphabet > len_alphabet:
+            position_alphabet = 0
+
+        text_plan += alphabet[position_alphabet]
+    print(f"The message {direction}d is {text_plan}")
+
+option_continue = "yes"
+while(option_continue == "yes"):
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    ceaser(text, shift, direction)
+    option_continue = input("Are you want continue yes or no?")
+
+
+
+    
