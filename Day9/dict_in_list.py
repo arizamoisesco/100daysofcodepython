aa995ee0101a3fe806bc4e09a3a4ff2cc506df19
Moises Ariza <arizamoises@gmail.com>
2021-04-22 21:31:28 -0500

    Se agrega script de diccionario en una lista

---
+++ Day9/dict_in_list.py
@@ -0,0 +1,24 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+def add_new_country(pais, cant_visit, lugares):
+    travel_log.append({
+        "country": pais,
+        "visits": cant_visit,
+        "cities": lugares
+    })
+
+add_new_country("Colombia",2, ["Caldas", "Bogota"])
