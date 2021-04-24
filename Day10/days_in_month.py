dff80bb982da7fbd3a31c3b84fbe646d48216b6d
Moises Ariza <arizamoises@gmail.com>
2021-04-23 22:36:20 -0500

    Creacion del script que determina la cantidad de dias que posee cualquier año teniendo en cuenta si es bisiesto o no

---
+++ Day10/days_in_month.py
@@ -0,0 +1,33 @@
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+  leap = is_leap(year) 
+  if leap == True:
+    month_days[1] = 29
+  else: 
+    month_days[1] = 28
+  
+  month -= 1
+
+  select_month = month_days[month]
+  return select_month
+
+
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
