36ef2041625adf0c7b3fce0cda1ac6dbc5f88250
Moises Ariza <arizamoises@gmail.com>
2021-05-04 16:12:42 -0500

    Se agrego la posibilidad de agregar otra operacion con la respuesta anterior y un nuevo numero

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -21,15 +21,22 @@ operations = {"+":add,
              "*":multiply,
              "/":divide
 }
-
+option_continue = "s"
+     
 num1 = int(input("¿Cual es el primer número?: "))
 num2 = int(input("¿Cual es el segundo número?: "))
 print("¿Qué operación desea realizar?: ")
 for key in operations:
     print(key)
-operation_symbol = input("Ingrese el simbolo de la operación que desea ralizar: ")
+operation_symbol = input("Ingrese el simbolo de la operación que desea realizar: ")
 
 select_operation = operations[operation_symbol]
-answer = select_operation(num1, num2)
+first_answer = select_operation(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+num3 = int(input("¿Cuál es el siguiente numero"))
+operation_symbol = input("Escriba la siguiente operacion: ").lower()
+select_operation = operations[operation_symbol]
+second_answer = select_operation(select_operation(num1, num2), num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
